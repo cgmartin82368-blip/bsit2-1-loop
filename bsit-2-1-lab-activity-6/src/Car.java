d645295b6b3f86922495e5487eaa8ba6288ec9c3
Clark Gabriel Martin <cgmartin82368@liceo.edu.ph>
2026-08-13 09:29:29 +0800

    Lab Activity 6 - Inheritance

---
+++ bsit-2-1-lab-activity-6/src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    public class Car extends Vehicle {
+
+        // TODO 1: Add a field only cars have:
+        //         private int numberOfDoors;
+
+        // TODO 2: Create the constructor:
+        //   public Car(String brand, int year, int numberOfDoors) {
+        //       super(brand, year);      // reuse Vehicle's constructor
+        //       this.numberOfDoors = numberOfDoors;
+        //   }
+
+        // TODO 3: Override displayInfo() with @Override so it prints:
+        //   Car: <brand> (<year>) - <numberOfDoors> doors
+    }
+
+}
